cc07f50ceb2be81e44f606f03c6960ed48cad400
Espen Moe-Nilssen <espen@medialog.no>
2015-12-08 19:02:25 +0100

    more magic from davisagli

--- medialog/leadimagesize/behavior.py
+++ medialog/leadimagesize/behavior.py
@@ -5,23 +5,18 @@ from zope.interface import alsoProvides
 from zope.i18nmessageid import MessageFactory
 
 from plone import api
-#from medialog.leadimagesize.interfaces import ILeadImageSizeSettings
 
 _ = MessageFactory('medialog.leadimagesizes')
  
 class ICustomSize(form.Schema):
     """ A field where you can set the size for lead image"""
     
-    def defaultleadsize():
-        """Returns default settings"""
-        return  api.portal.get_registry_record('medialog.leadimagesize.interfaces.ILeadImageSizeSettings.leadsize')
-            
     leadsize = schema.Choice(
         title = _("label_leadimagesize", default=u"Image Size"),
         description = _("help_leadimagesize",
                       default="Choose Size"),
         vocabulary='medialog.leadimagesize.LeadImageSizeVocabulary',
-        defaultFactory=defaultleadsize,
+        defaultFactory=lambda: api.portal.get_registry_record('medialog.leadimagesize.interfaces.ILeadImageSizeSettings.leadsize') ,
     )
 
 alsoProvides(ICustomSize, IFormFieldProvider)
