bfbba0c96f8bdec37c6f9fd2ef80e7355bfa67e9
Espen Moe-Nilssen <espen@medialog.no>
2019-03-12 15:30:19 +0100

    typo

--- medialog/leadimagesize/browser/views.py
+++ medialog/leadimagesize/browser/views.py
@@ -1,5 +1,4 @@
-#from cStringIO import StringIO
-from io import StrinIO
+from io import StringIO
 from PIL import Image
 from PIL.PngImagePlugin import PngImageFile
 
@@ -10,17 +9,16 @@ from zope import schema
 
 class IRotateR(Interface):
     """ Rotate Image view interface """
-    
+
     def test():
         """ test method"""
 
 class RotateR(BrowserView):
     """ A browser view to rotate image """
-    
+
     def __call__(self, REQUEST):
         im = Image.open(StringIO(self.context.image.data))
         im.rotate(90)
         self.context.image =  im
         self.context.image.rotate(90)
         self.context.reindexObject()
-        
